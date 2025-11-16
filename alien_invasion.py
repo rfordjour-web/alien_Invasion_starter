ff9f79c0c8cd20dbbba242290eee7994bafbac1b
rfordjour-web <rforjour@student.cscc.edu>
2025-11-15 19:51:19 -0500

    set up AlienInvasion class structure and run_game function.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,12 @@
 import sys
 import pygame
+from settings import Settings 
+from ship import Ship
+
+
+
+
+
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -7,22 +14,49 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game and create game resources."""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+            )
+
+        pygame.display.set_caption(self.settings.name)
+
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(self.bg, (
+            self.settings.screen_width, self.settings.screen_height)
+            )
+
+
 
         self.running = True
+
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
         while self.running:
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
                     pygame.quit()
                     sys.exit()
 
+
+
+            self.screen.blit(self.bg, (0,0))   # Draw the background image to the screen    
+            self.ship.draw()  # Draw the ship at its current location 
+
             
-            pygame.display.flip()
+            pygame.display.flip() # Update the screen
+            
+
+
+
+
+
+            self.clock = pygame.time.Clock()  # Limit the frame rate to 60 FPS
 
 
 
---
+++ settings.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.name = "Alien Invasion"  # Removed type hint
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.FPS = 60
+        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'starbasesnow.png'
+        self.bg_color = (230, 230, 230)
+        
+        self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship2python alien_invasion.py
+.png'
+        self.ship_width = 40
+        self.ship_height = 60
