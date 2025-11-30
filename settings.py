94622363049b9dcaed156bc45aa3323419782ec5
rfordjour-web <rforjour@student.cscc.edu>
2025-11-30 17:24:28 -0500

    adjustments

--- settings.py
+++ settings.py
@@ -27,20 +27,19 @@ class Settings:
         self.ship_file = str(Path.cwd() / 'Assets' / 'images'/ 'ship2(no bg).png')
         self.ship_width = 30
         self.ship_height = 44
-        self.ship_speed = 1
-        self.starting_ship_count = 5  # start with 5 lives
+        self.ship_speed = 7
+        self.starting_ship_count = 5
 
         # ------------------------
         # Bullet settings
         # ------------------------
         self.bullet_width = 7
         self.bullet_height = 25
-        self.bullet_speed = 10
+        self.bullet_speed = 12
         self.bullets_allowed = 10
         self.bullet_color = (255, 255, 255)
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
         self.impact_sound = Path.cwd() / 'Assets' / 'sound' / 'impactSound.mp3'
-
 
         # ------------------------
         # Alien settings
@@ -48,11 +47,32 @@ class Settings:
         self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
         self.alien_width = 40
         self.alien_height = 40
-        self.fleet_speed = 0.35
-        self.fleet_direction = 1  # 1 represents right; -1 represents left
-        self.fleet_drop_speed = 1
+        self.fleet_speed = 0.3
+        self.fleet_direction = 1
+        self.fleet_drop_speed = 2
+        self.aliens_per_row = 6  # fewer aliens
+        self.alien_rows = 3       # fewer rows
+
+        # ------------------------
+        # Difficulty
+        # ------------------------
+        # Options: "easy", "medium", "hard"
+        self.difficulty = "easy"
+        self.adjust_for_difficulty()
 
         # ------------------------
         # Alien drum intro
         # ------------------------
-        self.alien_drums = Path.cwd() / 'Assets' / 'sound'/'alien_drums.wav'
+        self.alien_drums = Path.cwd() / 'Assets' / 'sound' / 'alien_drums.wav'
+
+    def adjust_for_difficulty(self):
+        """Adjust game parameters based on difficulty."""
+        if self.difficulty == "easy":
+            self.fleet_speed = 0.2
+            self.bullets_allowed = 15
+        elif self.difficulty == "medium":
+            self.fleet_speed = 0.35
+            self.bullets_allowed = 10
+        elif self.difficulty == "hard":
+            self.fleet_speed = 0.5
+            self.bullets_allowed = 7
